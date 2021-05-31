89c2131c5d575ed1e4edd29a953afae1d48330a2
Ahmed Yehia <ahmedyehia.edu@gmail.com>
2021-05-31 18:20:41 +0200

    second implementation

--- keyframe.py
+++ keyframe.py
@@ -1,38 +1,54 @@
-import cv2
+from cv2 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import time
+from time import time
+import sys
 
-cap = cv2.VideoCapture('footage4.mp4')
-frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))-1
-frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+def keyframe(name,threshold=0.3):
+    cap = cv2.VideoCapture(name)
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))-1
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-ret,current = cap.read()
-frameDiff = np.zeros((frameCount,1))
-keyframes = [cv2.cvtColor(current,cv2.COLOR_BGR2RGB)]
+    ret,current = cap.read()
+    prev = current
+    keyframes = [cv2.cvtColor(prev,cv2.COLOR_BGR2RGB)]
 
-currentHist = cv2.calcHist([cv2.cvtColor(current,cv2.COLOR_RGB2GRAY)],[0],None,[256],[0,256])
+    prevHist = np.array([cv2.calcHist([current],[0],None,[256],[0,256]),
+                         cv2.calcHist([current],[1],None,[256],[0,256]),
+                         cv2.calcHist([current],[2],None,[256],[0,256])])
 
+    for dummy in range(1,frameCount):
+        if ret:
+            ret, current = cap.read()
+            currentHist = np.array([cv2.calcHist([current],[0],None,[256],[0,256]),
+                                    cv2.calcHist([current],[1],None,[256],[0,256]),
+                                    cv2.calcHist([current],[2],None,[256],[0,256])])
+            dif = np.sum(np.abs(prevHist-currentHist))/frameWidth/frameHeight/3
+            if dif > threshold:
+                keyframes.append(cv2.cvtColor(current,cv2.COLOR_BGR2RGB))
+            prev = current
+            prevHist = currentHist
 
-for fc in range(1,frameCount):
-    if ret:
-        ret, current = cap.read()
-        currentHist = cv2.calcHist([cv2.cvtColor(current,cv2.COLOR_RGB2GRAY)],[0],None,[256],[0,256])
-        frameHist = cv2.calcHist([cv2.cvtColor(keyframes[-1],cv2.COLOR_RGB2GRAY)],[0],None,[256],[0,256])
-        dif = np.sum(np.abs(frameHist-currentHist))/frameWidth/frameHeight/3
-        print(fc,dif)
-        if dif > 0.3:
-            keyframes.append(cv2.cvtColor(current,cv2.COLOR_BGR2RGB))
-            fig, axs = plt.subplots(1,2)
-            fig.suptitle(f"{dif} error in frame {fc}")
-            axs[0].imshow(keyframes[-2])
-            axs[1].imshow(keyframes[-1])
-            plt.show()
-        frameDiff[fc] = dif
+    cap.release()
+    return np.array(keyframes)
 
-cap.release()
+def show(keys):
+    n = len(keys)
+    c = int(np.sqrt(n))
+    r = int(np.ceil(n/c))
+    fig,a = plt.subplots(r,c,figsize=(20,20))
+    fig.suptitle('Keyframes')
+    for i in range(n):
+        a[int(i/c)][i%c].imshow(keys[i])
+        a[int(i/c)][i%c].axis('off')
+    for i in range(n,r*c):
+        a[int(i/c)][i%c].axis('off')
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.savefig("keyframes.png")
+    plt.show()
 
-print(len(keyframes))
-plt.plot(frameDiff)
-plt.show()
+start = time()
+keys = keyframe("footage.mp4")
+print(f"time taken: {time()-start} seconds")
+show(keys)
